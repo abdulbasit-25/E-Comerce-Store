0ca238881ab145b1cf58623de42afd600d4a3d09
abdulbasit-25 <abdulbasit.alpha25@gmail.com>
2026-08-23 11:36:20 +0500

    refactor: connect centralized theme styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
-import appCss from "../styles.css?url";
+import appCss from "../styles/theme.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
